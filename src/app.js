5470884b6e31731673fd3fd872483d7646fcfe33
Katerina Zenchik <e.zenchik@gmail.com>
2019-02-26 22:02:08 +0300

    Task1: show orders data in table

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
-// this is an example of improting data from JSON
-//import 'orders' from '../data/orders.json';
 const orders = require('../data/orders.json');
 
+
 export default (function () {
-    // YOUR CODE GOES HERE
+
+  const all_orders = Object.values(orders);
+  const columns = ["transaction_id", "user_id", "created_at", "total", "card_number", "card_type"]; //, "order_country", "order_ip"];
+
+  all_orders.forEach(function(order, i, all_orders) {
+
+    const tr = document.createElement('tr');
+    const order_id = "order_"+order.id;
+    tr.setAttribute("id", order_id);
+
+    for(const key in columns) {
+      const td = document.createElement("td");
+      console.log(key, columns, order[columns[key]]);
+      td.textContent = order[columns[key]];
+      tr.appendChild(td);
+    };
+
+    const td = document.createElement("td");
+    td.textContent = `${order["order_country"]} (${order["order_ip"]})`;
+    tr.appendChild(td);
+
+    document.getElementById("tbody").appendChild(tr);
+  });
+
+
     // next line is for example only
-    document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
+    document.getElementById("app");
 }());
