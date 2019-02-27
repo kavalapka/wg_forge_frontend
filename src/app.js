5796292c5ce350a6304a91036ed6f21f80abdfb1
Katerina Zenchik <e.zenchik@gmail.com>
2019-02-27 16:34:09 +0300

    Task2: format name

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const orders = require('../data/orders.json');
+const users = require('../data/users.json');
 
 
 function formatTimestamp(timestamp){
@@ -34,19 +35,38 @@ function formatCurrency(item) {
   return formatter.format(item);
 }
 
+function formatUser(user_data) {
+  var title;
+
+  switch(user_data.gender) {
+    case 'Male':
+      title = 'Mr.';
+      break;
+    case 'Female':
+      title = 'Ms.';
+      break;
+  }
+
+  return `${title}  ${user_data.first_name} ${user_data.last_name}`
+}
+
+
 export default (function () {
 
   const all_orders = Object.values(orders);
   const columns = ["transaction_id", "user_id", "created_at", "total", "card_number", "card_type"]; //, "order_country", "order_ip"];
+  const all_users = Object.values(users);
 
   all_orders.forEach(function(order, i, all_orders) {
 
     const tr = document.createElement('tr');
     tr.setAttribute("id", "order_"+order.id);
+    const user_data = all_users.find(user_data => user_data.id === order["user_id"]);
 
     order["created_at"] = formatTimestamp(order["created_at"]);
     order["card_number"] = formatCard(order["card_number"]);
     order["total"] = formatCurrency(order["total"]);
+    order["user_id"] = formatUser(user_data);
 
     for(const key in columns) {
       const td = document.createElement("td");
